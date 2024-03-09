1d6340de7e1d1985a5a83497bd89a231a819c729
umsimpleti <107601608+umsimplesfdp@users.noreply.github.com>
2024-03-09 17:12:58 -0300

    Create ex_001.c

---
+++ Primeiro semestre/Lógica de Programaçãol/C/Lista 01/ex_001.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, soma;
+	
+	printf("Informe o valor de A: "); // Comando de saída
+	scanf("%d", &a); // Comando de entrada
+	
+	printf("Informe o valor de B: ");
+	scanf("%d", &b);
+	
+	printf("Informe o valor de C: ");
+	scanf("%d", &c);
+	
+	soma = a + b;
+	
+	/*system("pause");
+	system("cls");*/
+	
+	if (soma < c) {
+		printf("A soma de A mais B eh menor do que C\n");
+		printf("%d < %d", soma, c );
+	}
+}

ad325e34e2d77cb8eb841c66c8c34ea88f27877c
umsimplerodrigo <107601608+umsimplesrodrigo@users.noreply.github.com>
2024-10-31 12:01:22 -0300

    Create ex_002.sql

---
+++ segundo_semestre/banco_de_dados/mysql/revisando/ex_002.sql
@@ -0,0 +1,42 @@
+-- Considere uma tabela vendas com as colunas id_venda, id_cliente, produto e valor.
+-- Escreva uma consulta para listar cada id_cliente, seguido de uma lista de todos os produtos comprados por esse cliente. Ordene os clientes por id_cliente em ordem crescente.
+
+CREATE TABLE clientes (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    cidade VARCHAR(255)
+);
+
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    valor DECIMAL(10,2),
+    FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente)
+);
+
+CREATE TABLE vendas (
+    id_venda INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    produto VARCHAR(255),
+    valor DECIMAL(10,2),
+    FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente)
+);
+
+INSERT INTO clientes (nome, cidade)
+VALUES ('Rodrigo Oliveira', 'Feira de Santana'),
+    ('Gabriel Menezes', 'Feira de Santana'),
+    ('Alexsandro Lira', 'Feira de Santana');
+
+INSERT INTO vendas (id_cliente, produto, valor)
+VALUES (1, 'Maconha', 10.20),
+    (2, 'Cocaina', 66.80),
+    (3, 'Crack', 16.90),
+    (1, 'Cocaina', 66.80),
+    (2, 'Maconha', 10.20),
+    (3, 'Cocaina', 66.80);
+
+SELECT c.nome, GROUP_CONCAT(v.produto ORDER BY v.produto ASC SEPARATOR ', ') AS produto
+FROM clientes AS c
+JOIN vendas AS v
+ON v.id_cliente = c.id_cliente
+GROUP BY c.nome;
